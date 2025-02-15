c71a83398b0cd2f5c3484225115974c2047d717b
Tom Brand <tom9358@gmail.com>
2025-02-15 09:59:12 +0100

    Model save path by default contains model name now

--- config.py
+++ config.py
@@ -14,14 +14,14 @@ similar_lang_nllb = 'nld_Latn'
 TATOEBA_PATH = 'tatoeba'
 modelpath = 'hfacemodels'
 timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-MODEL_SAVE_PATH = f'models/nllb-{"-".join(source_langs_tatoeba)}-distilled-1.3B-{timestamp}'
+MODEL_SAVE_PATH = f'models/{modelname.split('/')[-1]}-{"-".join(source_langs_tatoeba)}-{timestamp}'
 
 # Training parameters
 batch_size = 60
 max_chars = 200 # can be set to None
 max_length = 99 # tokens
 warmup_steps = 100
-training_steps = 40#int(2000 * 8 / batch_size)
+training_steps = int(2000 * 8 / batch_size)
 
 print('Model save path:', MODEL_SAVE_PATH)
 
