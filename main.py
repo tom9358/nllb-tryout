16dd8db07d96115165187b800293abeef9296e92
Tom Brand <tom.brand@tno.nl>
2025-03-29 14:36:38 +0000

    add tryout to main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from downloadtatoeba import main_download
 from corpus import main_corpus
 from train import main_train
 from evaluate import main_evaluate
+from tryout import main_tryout
 
 def main():
     # Step 1: Download data
@@ -14,8 +15,11 @@ def main():
     # Step 3: Train the model
     main_train(corpus_objects)
     
-    # # Step 4: Evaluate the model
-    main_evaluate(corpus_objects, MODEL_SAVE_PATH, new_lang_nllb)
+    # Step 3.5: Try out the model! Evaluation by means of vibes
+    main_tryout(MODEL_SAVE_PATH, new_lang_nllb)
+    
+    # Step 4: Evaluate the model
+    # main_evaluate(corpus_objects, MODEL_SAVE_PATH, new_lang_nllb)
 
 if __name__ == "__main__":
     main()
