12d374f92264bcdbdd0f478dab93dd5c38b55c4d
Tom9358 <tom9358@gmail.com>
2025-09-01 22:13:06 +0200

    update tokenizer setup to method of modern transformers versions. rest of code not checked yet, not tested yet.

--- src/nllb_try/tokenizer_and_model_setup.py
+++ src/nllb_try/tokenizer_and_model_setup.py
@@ -3,25 +3,6 @@ from gc import collect
 import torch
 from .config import config
 
-def fix_tokenizer(tokenizer, new_lang: str):
-    """
-    Add a new language token to the tokenizer vocabulary.
-    This should be done each time after its initialization.
-    We need transformers<=4.33 for this to work.
-    """
-    old_len = len(tokenizer) - int(new_lang in tokenizer.added_tokens_encoder)
-    tokenizer.lang_code_to_id[new_lang] = old_len - 1
-    tokenizer.id_to_lang_code[old_len - 1] = new_lang
-    tokenizer.fairseq_tokens_to_ids["<mask>"] = len(tokenizer.sp_model) + len(tokenizer.lang_code_to_id) + tokenizer.fairseq_offset
-    tokenizer.fairseq_tokens_to_ids.update(tokenizer.lang_code_to_id)
-    tokenizer.fairseq_ids_to_tokens = {v: k for k, v in tokenizer.fairseq_tokens_to_ids.items()}
-    
-    if new_lang not in tokenizer._additional_special_tokens:
-        tokenizer._additional_special_tokens.append(new_lang)
-
-    tokenizer.added_tokens_encoder = {}
-    tokenizer.added_tokens_decoder = {}
-
 def cleanup():
     """Try to free some memory."""
     collect()
@@ -40,15 +21,14 @@ def setup_model_and_tokenizer(modelname: str, modelpath: str = None, new_lang: s
     """
     device = torch.device(device)# if torch.cuda.is_available() else "cpu")
 
-    # Load the tokenizer
-    tokenizer = NllbTokenizer.from_pretrained(modelname, cache_dir=modelpath)
-    
-    # Fix the tokenizer if a new language is provided
     if new_lang:
-        fix_tokenizer(tokenizer, new_lang)
-        cleanup()
+        tokenizer = NllbTokenizer.from_pretrained(
+            modelname,
+            cache_dir=modelpath,
+            additional_special_tokens=[new_lang]
+        )
     else:
-        print('Fyi: No new language added')
+        tokenizer = NllbTokenizer.from_pretrained(modelname, cache_dir=modelpath)
 
     # Load the model
     model = AutoModelForSeq2SeqLM.from_pretrained(modelname, cache_dir=modelpath, device_map={"": str(device)})
