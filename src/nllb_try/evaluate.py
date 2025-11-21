d88b09b5e01a3a114bc9e69f6f5776cf68f64dba
Tom Brand <tom9358@gmail.com>
2025-11-21 10:34:22 +0000

    Add some training plots

--- src/nllb_try/evaluate.py
+++ src/nllb_try/evaluate.py
@@ -103,7 +103,7 @@ def _calculate_metrics_for_split(df_split: pd.DataFrame, src_lang_nllb: str, tgt
     }
 
 # Main evaluation function calls the helper for each split
-def evaluate_model(model, tokenizer, corpus_objects):
+def evaluate_model(model, tokenizer, corpus_objects, sample_size: int = 200):
     all_corpus_results = []
     for corpus in corpus_objects:
         corpus_results = {}
@@ -112,14 +112,14 @@ def evaluate_model(model, tokenizer, corpus_objects):
 
         # Evaluate on Training Set
         train_metrics = _calculate_metrics_for_split(
-            corpus.df_train, corpus.source_lang_nllb, corpus.target_lang_nllb, model, tokenizer
+            corpus.df_train, corpus.source_lang_nllb, corpus.target_lang_nllb, model, tokenizer, sample_size = sample_size
         )
         for k, v in train_metrics.items():
             corpus_results[f"train_{k}"] = v
 
         # Evaluate on Validation Set
         validate_metrics = _calculate_metrics_for_split(
-            corpus.df_validate, corpus.source_lang_nllb, corpus.target_lang_nllb, model, tokenizer
+            corpus.df_validate, corpus.source_lang_nllb, corpus.target_lang_nllb, model, tokenizer, sample_size = sample_size
         )
         for k, v in validate_metrics.items():
             corpus_results[f"validate_{k}"] = v
