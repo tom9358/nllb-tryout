c440f641c7980e074756e18acadc8a20abf7b7db
Tom Brand <tom9358@gmail.com>
2026-02-05 14:55:10 +0000

    Fix type hinting and add some debug print statements (preparing to update to transformers version 5.0.0)

--- src/nllb_try/tokenizer_and_model_setup.py
+++ src/nllb_try/tokenizer_and_model_setup.py
@@ -8,18 +8,24 @@ def cleanup():
     collect()
     torch.cuda.empty_cache()
 
-def setup_model_and_tokenizer(modelname: str, modelpath: str = None, new_lang: str = None, similar_lang: str = None, device: str = config['device']):
+def setup_model_and_tokenizer(
+    modelname: str,
+    modelpath: str | None = None,
+    new_lang: str | None = None,
+    similar_lang: str | None = None,
+    device: str = config["device"],
+):
     """
     Set up the model and tokenizer for use. This function handles both loading models from HuggingFace and local models,
     adjusts the tokenizer so a new language is supported and optionally sets embedding weights to those of a similar language.
-    
+
     Args:
     - modelname: The model identifier or path.
     - modelpath: The local directory path for caching/loading models.
     - new_lang: Optional; The new language code to add. (In nllb format, e.g. 'gos_Latn')
     - similar_lang: Optional; If provided, the similar language's weights initialize the new language. (nllb format)
     """
-    device = torch.device(device)# if torch.cuda.is_available() else "cpu")
+    torch_device = torch.device(device)# if torch.cuda.is_available() else "cpu")
 
     if new_lang:
         tokenizer = NllbTokenizer.from_pretrained(
@@ -31,8 +37,21 @@ def setup_model_and_tokenizer(modelname: str, modelpath: str = None, new_lang: s
         tokenizer = NllbTokenizer.from_pretrained(modelname, cache_dir=modelpath)
 
     # Load the model
-    model = AutoModelForSeq2SeqLM.from_pretrained(modelname, cache_dir=modelpath, device_map={"": str(device)})
-    model.resize_token_embeddings(len(tokenizer)) 
+    model = AutoModelForSeq2SeqLM.from_pretrained(modelname, cache_dir=modelpath, device_map={"": str(torch_device)})
+    model.resize_token_embeddings(len(tokenizer))
+    # model.tie_weights()
+
+    # Debug
+    shared_ptr = model.model.shared.weight.data_ptr()
+    enc_ptr = model.model.encoder.embed_tokens.weight.data_ptr()
+    dec_ptr = model.model.decoder.embed_tokens.weight.data_ptr()
+    lm_ptr = model.lm_head.weight.data_ptr()
+
+    print(f"Tied shared↔encoder: {shared_ptr == enc_ptr} (shared={shared_ptr}, enc={enc_ptr})")
+    print(f"Tied shared↔decoder: {shared_ptr == dec_ptr} (shared={shared_ptr}, dec={dec_ptr})")
+    print(f"Tied shared↔lm_head: {shared_ptr == lm_ptr} (shared={shared_ptr}, lm={lm_ptr})")
+    print("param dtype:", next(model.parameters()).dtype)
+    print("param device:", next(model.parameters()).device)
 
     # Check for the second situation: adding a new language with optional weights initialization
     if new_lang and similar_lang:
